3bd8b38b50add689d2b8bc247178ffffeed0f430
poisson <576031288@qq.com>
2021-12-25 10:45:44 +0800

    feign方式替代ribbon服务间调用

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ learn spring cloud together
 ##遇到的已解决的问题
 ###1.1 SpringCloud版本太高需要添加BootStrap的坐标，否则nacos的bootstrap.yml文件读取失败，导致一系列问题
 ###1.2 配置cluster.conf时候，记得用ip地址（浏览器输入ip即可），写127.0.0.1的话，会调用失败
+###1.3 很多问题都是因为各个库版本不兼容导致，最好的处理方式就是不手动指定版本，自动指定
--- order-service/src/main/java/com/ouest/order/service/OrderService.java
+++ order-service/src/main/java/com/ouest/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ouest.order.service;
 
+import com.ouest.order.clients.UserClient;
 import com.ouest.order.mapper.OrderMapper;
 import com.ouest.order.pojo.Order;
 import com.ouest.order.pojo.User;
@@ -13,14 +14,25 @@ public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderByOrderId(Long id) {
         Order order = orderMapper.selectById(id);
-        String url = "http://userservice/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        User user = userClient.findById(order.getUserId());
         order.setUser(user);
         return order;
     }
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public Order queryOrderByOrderId(Long id) {
+//        Order order = orderMapper.selectById(id);
+//        String url = "http://userservice/user/" + order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//        order.setUser(user);
+//        return order;
+//    }
 }
